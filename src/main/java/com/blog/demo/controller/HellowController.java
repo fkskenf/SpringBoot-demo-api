9af8b55db3ae0e7836f5af44617718f3bbcdc44d
hyeon <janghyeon6587@gmail.com>
2023-01-17 11:05:28 +0900

    Add : banner.text

--- src/main/java/com/blog/demo/controller/HellowController.java
+++ src/main/java/com/blog/demo/controller/HellowController.java
@@ -4,6 +4,7 @@ import com.blog.demo.domain.board;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +23,11 @@ public class HellowController {
     private final boardQueryRepository boardQueryRepository;
 
     @GetMapping("/insert")
-    public ResponseEntity<?> get() {
+    public ResponseEntity<?> get(@Value("${spring.config.activate.on-profile}") String test) {
 
+        logger.debug(test);
+        logger.debug(System.getProperty("spring.jpa.database"));
+        logger.debug(System.getProperty("log.config.path"));
         logger.debug("DEBUG");
         logger.info("INFO");
         board board2 = board.builder()
